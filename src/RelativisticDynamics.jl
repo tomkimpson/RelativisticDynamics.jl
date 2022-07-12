440cfde61e970a7f58a2a637c35c743c6abe2054
Tom Kimpson <tomkimpson@gmail.com>
2022-07-12 15:20:04 +0100

    first commit

--- src/RelativisticDynamics.jl
+++ src/RelativisticDynamics.jl
@@ -1,5 +1,8 @@
 module RelativisticDynamics
 
 # Write your package code here.
+#export greet_you_package_name 
+include("functions.jl")
 
+greet_your_package_name()
 end
---
+++ src/functions.jl
@@ -0,0 +1,3 @@
+function greet_your_package_name()
+    println("Hello from RelativisticDynamics.jl!")
+end
